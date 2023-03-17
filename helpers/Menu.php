c9421c3a01f97ac6d972a4ab97837d9e48a89b60
Arifatul Maghfiroh <102203155+Arifaaa@users.noreply.github.com>
2023-03-17 18:56:53 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'surat_keluar', 
+			'label' => 'Surat Keluar', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'surat_masuk', 
+			'label' => 'Surat Masuk', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'surat_keluar', 
+			'label' => 'Surat Keluar', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'surat_masuk', 
+			'label' => 'Surat Masuk', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
